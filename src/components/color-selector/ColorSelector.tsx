529d77a92735ae25e5fd5c8703d2b92beeda2869
nikitaKor98 <korytnikovnikita@mail.ru>
2024-09-22 18:37:45 +0400

    add changing color in colorSelector

--- src/components/color-selector/ColorSelector.tsx
+++ src/components/color-selector/ColorSelector.tsx
@@ -1,5 +1,160 @@
+import { useRef, useState } from "react";
+
 import Button from "components/button/Button";
-import { useState } from "react";
+
+const spectrumRanges = [
+    { from: [255, 0, 0], to: [255, 255, 0] },
+    { from: [255, 255, 0], to: [0, 255, 0] },
+    { from: [0, 255, 0], to: [0, 255, 255] },
+    { from: [0, 255, 255], to: [0, 0, 255] },
+    { from: [0, 0, 255], to: [255, 0, 255] },
+    { from: [255, 0, 255], to: [255, 0, 0] }
+];
+
+const hexToRgb = (hex: any) => {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result && {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    }
+}
+
+const setDarkOrLight = (hex: any) => {
+
+    const rgb = hexToRgb(hex);
+
+    if (rgb && Math.sqrt(0.299 * (rgb.r * rgb.r) + 0.587 * (rgb.g * rgb.g) + 0.114 * (rgb.b * rgb.b)) > 127.5) {
+        return "#333333";
+    } else {
+        return "#fff";
+    }
+}
+
+const rgbToHex = ({ r, g, b }: any) => {
+    return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+}
+
+const convertPxsToProcents = (value: number, maxValue: number): number => {
+    return Math.floor(100 * value / maxValue);
+}
+
+const convertProcentsToPxs = (value: number, maxValue: number): number => {
+    return Math.floor(value * maxValue / 100);
+}
+
+const findColor = (from: any, to: any, leftDistRatio: any) => {
+    return Math.round(from + (to - from) * leftDistRatio);
+}
+
+const findRgbFromMouse = (event: any, ref: any, spectrum: any) => {
+    const { left, width } = ref.current.getBoundingClientRect();
+    const leftDistance = Math.min(Math.max(event.clientX - left, 0), width - 1);
+    const rangeWidth = width / spectrum.length;
+    const includedRange = Math.floor(leftDistance / rangeWidth);
+    const leftDistRatio = ((leftDistance % rangeWidth) / rangeWidth).toFixed(2);
+    const { from, to } = spectrum[includedRange];
+    return {
+        r: findColor(from[0], to[0], leftDistRatio),
+        g: findColor(from[1], to[1], leftDistRatio),
+        b: findColor(from[2], to[2], leftDistRatio)
+    }
+}
+
+const darken = (color: any, ratio: any) => Math.round((1 - ratio) * color);
+const whiten = (color: any, ratio: any) => Math.round(color + (255 - color) * ratio);
+const adjustSaturation = ({ r, g, b }: any) => (ratio: any, adjustmentFn: any) => {
+    return {
+        r: adjustmentFn(r, ratio),
+        g: adjustmentFn(g, ratio),
+        b: adjustmentFn(b, ratio)
+    }
+}
+
+const saturate = (rgb: any, e: any, ref: any) => {
+    const { top, height, left, width } = ref.current.getBoundingClientRect();
+    const topDistance = Math.min(Math.max(e.clientY - top, 0), height);
+    const leftDistance = Math.min(Math.max(e.clientX - left, 0), width);
+    const topDistRatio: number = Number((topDistance / height).toFixed(2));
+    const leftDistRatio: number = Number((leftDistance / width).toFixed(2));
+    if (topDistRatio > 0) {
+        const darknessRatio = topDistRatio;
+        return adjustSaturation(rgb)(darknessRatio, darken);
+    }
+    if (leftDistRatio < 1) {
+        const whitenessRatio = (1 - leftDistRatio) / 1;
+        return adjustSaturation(rgb)(whitenessRatio, whiten);
+    }
+    return rgb;
+}
+
+function rgbToHsv({ r, g, b }: any) {
+    let rabs = r / 255;
+    let gabs = g / 255;
+    let babs = b / 255;
+    let v = Math.max(rabs, gabs, babs);
+    let diff = v - Math.min(rabs, gabs, babs);
+    let diffc = (c: any) => (v - c) / 6 / diff + 1 / 2;
+    let percentRoundFn = (num: any) => Math.round(num * 100 / 100);
+
+    let h, s;
+
+    if (diff == 0) {
+        h = s = 0;
+    } else {
+        s = diff / v;
+        let rr = diffc(rabs);
+        let gg = diffc(gabs);
+        let bb = diffc(babs);
+
+        if (rabs === v) {
+            h = bb - gg;
+        } else if (gabs === v) {
+            h = (1 / 3) + rr - bb;
+        } else if (babs === v) {
+            h = (2 / 3) + gg - rr;
+        }
+
+        if (Number(h) < 0) {
+            h = Number(h) + 1;
+        } else if (Number(h) > 1) {
+            h = Number(h) - 1;
+        }
+    }
+
+    return {
+        h: Math.round(Number(h) * 360),
+        s: percentRoundFn(s * 100),
+        v: percentRoundFn(v * 100)
+    };
+}
+
+function rgbToHsl({ r, g, b }: any) {
+    r = r / 255;
+    g = g / 255;
+    b = b / 255;
+    const max = Math.max(r, g, b), min = Math.min(r, g, b);
+    let h, s, l = (max + min) / 2;
+
+    if (max == min) {
+        h = s = 0;
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h = Number(h) / 6;
+    }
+
+    return {
+        h: Math.floor(h * 360),
+        s: Math.floor(s * 100),
+        l: Math.floor(l * 100)
+    };
+}
 
 function ColorSelector(props: any) {
 
@@ -88,26 +243,127 @@ function ColorSelector(props: any) {
         darkMagentaThree: "#4c1130"
     }
 
-    const [color, setColor] = useState("#fff");
+    const [colorСheckBox, setColorСheckBox] = useState("#fff");
+    const [backgroundColor, setBackgroundColor] = useState({ r: 255, g: 0, b: 0 });
+    const [activColor, setActivColor] = useState({ r: 255, g: 0, b: 0 });
+    const [spectrum, setSpectrum] = useState([
+        { from: [255, 255, 255], to: [255, 0, 0] }
+    ]);
     const [isOpen, setIsOpen] = useState(false);
+    const [swatchPointer, setSwatchPointer] = useState(0);
+    const [boxPointerLeft, setBoxPointerLeft] = useState(0);
+    const [boxPointerTop, setBoxPointerTop] = useState(0);
 
-    const checkBox = { "--checkBox-color": color } as React.CSSProperties;
+    const { r, g, b } = backgroundColor;
+
+    const boxRef = useRef<HTMLDivElement>(null);
+    const swatchSliderRef = useRef<HTMLDivElement>(null);
+    const boxPointerRef = useRef<HTMLDivElement>(null);
+    const swatchPointerRef = useRef<HTMLDivElement>(null);
+
+    const checkBox = { "--checkBox-color": colorСheckBox } as React.CSSProperties;
 
     const onClose = () => {
         setIsOpen(!isOpen)
     }
 
-    const setDarkOrLight = (hex: any) => {
-        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        const rgb = result && {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16)
+    const handelMouseDown = (event: any) => {
+
+        const { pageX, pageY } = event;
+        const { id } = event.target;
+
+        const handelOnClick = (event: any) => {
+            if (id === "box") {
+                setBoxPointerLeft(convertPxsToProcents(event.offsetX, boxRef.current?.clientWidth || 0));
+                setBoxPointerTop(convertPxsToProcents(event.offsetY, boxRef.current?.clientHeight || 0));
+                const rgb = findRgbFromMouse(event, boxRef, spectrum);
+                setActivColor(saturate(rgb, event, boxRef));
+            }
+
+            if (id === "slider") {
+                setSwatchPointer(convertPxsToProcents(event.offsetX, swatchSliderRef.current?.clientWidth || 0));
+                const rgb = findRgbFromMouse(event, swatchSliderRef, spectrumRanges);
+                setBackgroundColor(rgb);
+                setSpectrum([{ from: [...spectrum[0].from], to: [rgb.r, rgb.g, rgb.b] }]);
+            }
         }
-        if (rgb && Math.sqrt(0.299 * (rgb.r * rgb.r) + 0.587 * (rgb.g * rgb.g) + 0.114 * (rgb.b * rgb.b)) > 127.5) {
-            setColor("#333333");
-        } else {
-            setColor("#fff");
+
+        const handelMouseMove = (event: any) => {
+            const moveX = event.pageX - pageX + convertProcentsToPxs(swatchPointer, swatchSliderRef.current?.clientWidth || 0);
+            const moveBoxX = event.pageX - pageX + convertProcentsToPxs(boxPointerLeft, boxRef.current?.clientWidth || 0);
+            const moveBoxY = event.pageY - pageY + convertProcentsToPxs(boxPointerTop, boxRef.current?.clientHeight || 0);
+
+            if ((id === "swatchPointer" || id === "slider") && moveX >= 0 && convertPxsToProcents(moveX, swatchSliderRef.current?.clientWidth || 0) <= 100) {
+                const rgb = findRgbFromMouse(event, swatchSliderRef, spectrumRanges);
+                const { x, y } = boxRef.current?.getBoundingClientRect() || { x: 0, y: 0 };
+
+                setSwatchPointer(convertPxsToProcents(moveX, swatchSliderRef.current?.clientWidth || 0));
+                setBackgroundColor(rgb);
+                setSpectrum([{ from: [...spectrum[0].from], to: [rgb.r, rgb.g, rgb.b] }]);
+
+                setActivColor(saturate(rgb, {
+                    clientX: convertProcentsToPxs(boxPointerLeft, boxRef.current?.clientWidth || 0) + x,
+                    clientY: convertProcentsToPxs(boxPointerTop, boxRef.current?.clientHeight || 0) + y
+                }, boxRef));
+            }
+
+            if ((id === "boxPointer" || id === "box") && moveBoxX >= 0 && convertPxsToProcents(moveBoxX, boxRef.current?.clientWidth || 0) <= 100) {
+                setBoxPointerLeft(convertPxsToProcents(moveBoxX, boxRef.current?.clientWidth || 0));
+            }
+
+            if ((id === "boxPointer" || id === "box") && moveBoxY >= 0 && convertPxsToProcents(moveBoxY, boxRef.current?.clientHeight || 0) <= 100) {
+                setBoxPointerTop(convertPxsToProcents(moveBoxY, boxRef.current?.clientHeight || 0));
+            }
+
+            if (id === "boxPointer" || id === "box") {
+                const rgb = findRgbFromMouse(event, boxRef, spectrum);
+                setActivColor(saturate(rgb, event, boxRef));
+            }
+        }
+
+        const handelMouseUp = () => {
+            window.removeEventListener("mousedown", handelOnClick);
+            window.removeEventListener("mousemove", handelMouseMove);
+            window.removeEventListener("mouseup", handelMouseUp);
+        }
+
+        window.addEventListener("mousedown", handelOnClick);
+        window.addEventListener("mousemove", handelMouseMove);
+        window.addEventListener("mouseup", handelMouseUp);
+    }
+
+    console.log(rgbToHsv(activColor))
+
+    const handleInputColor = (event: any) => {
+        const { id, value } = event.target;
+
+        if (id === "hex") {
+            setActivColor(hexToRgb(value) || { r: 0, g: 0, b: 0 });
+            const { h, s, v } = rgbToHsv(activColor);
+            setSwatchPointer(convertPxsToProcents(h, 360));
+            setBoxPointerLeft(s);
+            setBoxPointerTop(100 - v);
+        }
+        if (id === "r") {
+            setActivColor({ ...activColor, r: Number(value) });
+            const { h, s, v } = rgbToHsv(activColor);
+            setSwatchPointer(convertPxsToProcents(h, 360));
+            setBoxPointerLeft(s);
+            setBoxPointerTop(100 - v);
+        }
+        if (id === "g") {
+            setActivColor({ ...activColor, g: Number(value) });
+            const { h, s, v } = rgbToHsv(activColor);
+            setSwatchPointer(convertPxsToProcents(h, 360));
+            setBoxPointerLeft(s);
+            setBoxPointerTop(100 - v);
+        }
+        if (id === "b") {
+            setActivColor({ ...activColor, b: Number(value) });
+            const { h, s, v } = rgbToHsv(activColor);
+            setSwatchPointer(convertPxsToProcents(h, 360));
+            setBoxPointerLeft(s);
+            setBoxPointerTop(100 - v);
         }
     }
 
@@ -120,10 +376,10 @@ function ColorSelector(props: any) {
                         key={color}
                         onClick={() => {
                             setValue(color);
-                            setDarkOrLight(colorData[color]);
+                            setColorСheckBox(setDarkOrLight(colorData[color]));
                         }}
-                        className={`color-selector__color-palette_item${value === color ? " color-selector__color-palette_item_active" : ""}`}
-                        style={{ backgroundColor: `${colorData[color]}`, ...checkBox }}
+                        className={`color-selector__color-palette_item${value === color ? " color-selector__color-palette_item_active" : ""} `}
+                        style={{ backgroundColor: `${colorData[color]} `, ...checkBox }}
                     ></li>
                 })}
             </ul>
@@ -131,17 +387,75 @@ function ColorSelector(props: any) {
                 onClick={onClose}
             ><p>Custom color</p></Button>
             {isOpen && <div className="color-selector__color-custom">
-                <div className="color-selector__color-custom_box">
-                    <div className="color-selector__color-custom_box_pointer"></div>
+                <div
+                    ref={boxRef}
+                    id="box"
+                    onMouseDown={handelMouseDown}
+                    style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}
+                    className="color-selector__color-custom_box">
+                    <div
+                        ref={boxPointerRef}
+                        id="boxPointer"
+                        style={{ left: `${boxPointerLeft}% `, top: `${boxPointerTop}% `, backgroundColor: `rgb(${activColor.r}, ${activColor.g}, ${activColor.b})` }}
+                        onMouseDown={handelMouseDown}
+                        className="color-selector__color-custom_box_pointer"></div>
                 </div>
                 <div className="color-selector__color-custom_slider">
-                    <div className="color-selector__color-custom_slider_swatch"></div>
-                    <div className="color-selector__color-custom_slider_swatch-slider">
-                        <div className="color-selector__color-custom_slider_swatch-slider_pointer"></div>
+                    <div
+                        style={{ backgroundColor: `rgb(${activColor.r}, ${activColor.g}, ${activColor.b})` }}
+                        className="color-selector__color-custom_slider_swatch"></div>
+                    <div
+                        ref={swatchSliderRef}
+                        id="slider"
+                        onMouseDown={handelMouseDown}
+                        className="color-selector__color-custom_slider_swatch-slider">
+                        <div
+                            ref={swatchPointerRef}
+                            id="swatchPointer"
+                            style={{ left: `${swatchPointer}% `, backgroundColor: `rgb(${r}, ${g}, ${b})` }}
+                            onMouseDown={handelMouseDown}
+                            className="color-selector__color-custom_slider_swatch-slider_pointer"></div>
                     </div>
                 </div>
-                <div className="color-selector__color-custom_information">
-
+                <div className="color-selector__color-custom-information">
+                    <div className="color-selector__color-custom-information-hex">
+                        <input
+                            className="hex-input"
+                            id="hex"
+                            value={rgbToHex(activColor)}
+                            onChange={handleInputColor}
+                        />
+                        <label>hex</label>
+                    </div>
+                    <div className="color-selector__color-custom-information-fields">
+                        <div className="color-selector__color-custom-information-fields_item-r">
+                            <input
+                                className="color-fields-input"
+                                id="r"
+                                value={`${activColor.r}`}
+                                onChange={handleInputColor}
+                            />
+                            <label>r</label>
+                        </div>
+                        <div className="color-selector__color-custom-information-fields_item-g">
+                            <input
+                                className="color-fields-input"
+                                id="g"
+                                value={`${activColor.g}`}
+                                onChange={handleInputColor}
+                            />
+                            <label>g</label>
+                        </div>
+                        <div className="color-selector__color-custom-information-fields_item-b">
+                            <input
+                                className="color-fields-input"
+                                id="b"
+                                value={`${activColor.b}`}
+                                onChange={handleInputColor}
+                            />
+                            <label>b</label>
+                        </div>
+                    </div>
                 </div>
             </div>}
         </div>
